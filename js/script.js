9796c1e96aed063aa955c8a78d49d1c208e12334
Andrey <redship@outlook.com>
2023-11-14 01:21:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/* alert('Hello'); */
+
+/* const result = confirm('Are you here?');
+console.log(typeof(result)); */
+
+/* let answer = +prompt('Вам есть 18?', '18');
+console.log(answer+5); */
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+answers[2] = prompt('Ваш возраст?', '');
+
+console.log(typeof(answers));
+
