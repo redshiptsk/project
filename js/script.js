15e9f2a5fe58a9fdefa1ba7fdcd061fabb8a5465
Andrey <redship@outlook.com>
2023-11-14 22:00:09 +0700

    first app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const movie1 = prompt("Один из последних просмотренных фильмов?", ""),
+    raiting1 = +prompt("Как оцениет его?", ""),
+    movie2 = prompt("Один из последних просмотренных фильмов?", ""),
+    raiting2 = +prompt("Как оцениет его?", "");
+    personalMovieDB.movies[movie1] = raiting1;
+    personalMovieDB.movies[movie2] = raiting2;
+console.log(personalMovieDB);
