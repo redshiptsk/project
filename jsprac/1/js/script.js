1b0655f4a2c082c91826543172d14aabd5a88a16
Andrey <redship@outlook.com>
2023-11-30 18:32:33 +0700

    ajax

--- jsprac/1/js/script.js
+++ jsprac/1/js/script.js
@@ -217,5 +217,53 @@ document.addEventListener('DOMContentLoaded', () => {
         'big'
     ).render();
 
+    const forms = document.querySelectorAll('form');
 
+    const message = {
+        loading: "Загрузка",
+        succsess: "Успех",
+        failure: "Ошибка"
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value
+            })
+
+            const json = JSON.stringify(object)
+
+            request.send(json);
+            request.addEventListener('load', () =>{
+                if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.succsess;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            })
+        });
+    }
 });
